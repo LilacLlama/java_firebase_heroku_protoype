db9bd54af67b2911e11948d1a1fbd0409a6c4dff
LilacLlama <8921522+LilacLlama@users.noreply.github.com>
2020-11-14 13:37:06 -0500

    Spring Boot Initializer starter project

---
+++ src/test/java/com/lilacllama/springboot_firebase_heroku_prototype/SpringbootFirebaseHerokuPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilacllama.springboot_firebase_heroku_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirebaseHerokuPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
