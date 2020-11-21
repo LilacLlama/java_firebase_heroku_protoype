b509264f939f478540c4066a9d0d9a20da1677c4
LilacLlama <8921522+LilacLlama@users.noreply.github.com>
2020-11-20 19:01:58 -0500

    Updating to include firebase db ingregration w/ thymeleaf display

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# java_firebase_heroku_protoype
+# Spring Boot, Java 11, Firebase on Heroku Prototype
+
 Sandbox example project working with Java &amp; Firebase on Heroku
+
+## Setup & Requirements
+
+- [java 11](https://openjdk.java.net/projects/jdk/11/) > `brew install openjdk@11`
+- [maven](https://maven.apache.org/) > `brew install maven`
+- [Heroku & Heroku cli](https://devcenter.heroku.com/articles/heroku-cli) > `brew tap heroku/brew && brew install heroku`
+- [Firebase Project](https://firebase.google.com/docs/projects/learn-more)
+  - [associated firestore](https://firebase.google.com/docs/firestore)
+    - new collection > todos
+      - Todo > priority / description / created_date / due_date
+  - [firebase & service account secrets](https://stackoverflow.com/questions/37418372/firebase-where-is-my-account-secret-in-the-new-console)
+    - local development save in .env file / on heroku save as config variable
+      - SERVICE_JSON > this is your google credentials JSON
+      - DB_URL > your firestore url
+
+### How to Run
+
+1. clone repo
+2. ensure requirements are met
+3. login to Heroku cli & use `heroku create` inside repository folder
+4. To run locally
+   - ensure .env is filled out
+   - `mvn clean compile package`
+   - `heroku local` > live @ localhost:5000
+5. To run on heroku
+   - ensure config variables filled out
+   - `git push heroku main`
+   - `heroku open` > open in browser
+
+
+
+## Working with Java & Firebase
+
--- src/main/java/com/lilacllama/prototype/app/MainController.java
+++ src/main/java/com/lilacllama/prototype/app/MainController.java
@@ -1,5 +1,21 @@
 package com.lilacllama.prototype.app;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Date;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,14 +29,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @CrossOrigin
 public class MainController {
 
-    public MainController() {
+    Firestore db;
+
+    public MainController() throws Exception {
+        String jsonData = System.getenv("SERVICE_JSON");
+        String dbURL = System.getenv("DB_URL");
+        System.out.println("JSONDATA::" + jsonData);
+        System.out.println("DB_URL::" + jsonData);
+        InputStream serviceAccount = new ByteArrayInputStream(jsonData.getBytes());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount)).setDatabaseUrl(dbURL).build();
+
+        FirebaseApp.initializeApp(options);
+        this.db = FirestoreClient.getFirestore();
+        // asynchronously retrieve all users
     }
 
     @GetMapping({ "/", "/home" })
     public String adminConsole(Model model) throws Exception {
         System.out.println("Root.");
+        ApiFuture<QuerySnapshot> query = db.collection("todos").get();
+        // ...
+        // query.get() blocks on response
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        List<Map> dataList = new ArrayList<>();
+        for (QueryDocumentSnapshot document : documents) {
+            Map docData = document.getData();
+            docData.put("id", document.getId());
+            System.out.println(docData.toString());
+            dataList.add(docData);
+        }
 
-        model.addAttribute("dataList", new String[] { "Red", "Blue", "Green" });
+        model.addAttribute("dataList", dataList);
         return "main";
     }
 
